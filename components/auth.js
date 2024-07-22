41129efdf8e546b6be9af6030975427584ae1289
kwangyoulsagong <sgky0511@naver.com>
2024-07-22 16:48:57 +0900

    feat: 회원가입 및 로그인 api 구축: 회원가입시 데이터베이스에 저장 로그인시 데이터 검증을 통하여 accesstoken 닉네임 refreshtoken 발급

---
+++ components/auth.js
@@ -0,0 +1,38 @@
+module.exports = {
+    UserSignup: {
+      type: "object",
+      properties: {
+        id: {
+          type: "string",
+          example: "ktb23@kakao.com",
+        },
+        password: {
+          type: "string",
+          example: "ktb1234",
+        },
+        nickname: {
+            type: "string",
+            example: "닉네임",
+          },
+        weight: {
+            type: "float",
+            example: "68",
+        },
+      },
+      required: ["id", "password","nickname","weight"],
+    },
+    UserLogin: {
+      type: "object",
+      properties: {
+        id: {
+          type: "string",
+          example: "ktb23@kakao.com",
+        },
+        password: {
+          type: "string",
+          example: "ktb1234",
+        },
+      },
+      required: ["id", "password"],
+    },
+  };
