59adb164f0e2eb1e3f4209ddeecf1d86e1beb02c
kwangyoulsagong <sgky0511@naver.com>
2024-08-16 14:14:24 +0900

    feat: 프로필 수정 컨트롤러: 프로필 수정시 컨트롤러를 통해 받은 값들을 서비스에게 전달 후 현재 상태를 받고 다시 클라언트로 전달 #9

---
+++ services/profile.service.js
@@ -0,0 +1,45 @@
+const connection = require("../db");
+// 공통 쿼리 실행 함수
+const executeQuery = (query, params) => {
+  return new Promise((resolve, reject) => {
+    connection.query(query, params, (err, res) => {
+      if (err) return reject(err);
+      resolve(res);
+    });
+  });
+};
+
+// 프로필 수정 서비스
+const UpdateProfileService = async (
+  profile_id,
+  user_id,
+  id,
+  name,
+  statusMessage,
+  result
+) => {
+  // 프로필 테이블 업데이트 쿼리
+  const updateProfileQuery =
+    "UPDATE profile_tb SET statusMessage=? WHERE profile_id = ? AND user_id = ?";
+
+  // 유저 테이블 업데이트 쿼리
+  const updateUserQuery = "UPDATE user_tb SET name=? id=? WHERE user_id=?";
+  try {
+    // profile_tb 업데이트
+    await executeQuery(updateProfileQuery, [
+      statusMessage,
+      profile_id,
+      user_id,
+    ]);
+
+    // user_tb 업데이트
+    await executeQuery(updateUserQuery, [name, id, user_id]);
+    result(null, { message: "프로필 및 이름 수정 성공" });
+  } catch (error) {
+    console.error("프로필 및 이름 수정 중 에러 발생", error);
+    result(error, null);
+  }
+};
+module.exports = {
+  UpdateProfileService,
+};
