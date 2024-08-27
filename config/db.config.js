c1195682fc90381ddcd553ec66f700dbdc789d04
parkseyoung <hellosy76@naver.com>
2024-08-27 13:42:21 +0900

    docs: 오류

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  HOST: "healthcarebe.cx2ak0ukamvz.ap-northeast-2.rds.amazonaws.com",
+  HOST: "localhost",
   USER: "ktb23",
   PASSWORD: "!ktb1234",
   DB: "healthdb",
-  PORT: 3306     
 };
