204e289f2d282a463b1cfe380d68f93eb8858091
kwangyoulsagong <sgky0511@naver.com>
2024-09-11 13:15:48 +0900

    feat: 음식 기록 그래프 데이터 서버 : #36

--- controllers/graph.controller.js
+++ controllers/graph.controller.js
@@ -48,4 +48,28 @@ const WeightGraph = async (req, res) => {
     return res.status(500).json({ message: "서버 내부 오류" });
   }
 };
-module.exports = { ExerciseGraph, WeightGraph };
+
+const FoodGraph = async (req, res) => {
+  if (!req.user) {
+    res.status(401).json({ message: "인증 권한 없음" });
+    return;
+  }
+  const userId = req.user.user_id;
+  const { type, date } = req.query;
+  try {
+    if (type === "weekly" && date) {
+      await graphService.getWeeklyFoodData(userId, date, (err, data) => {
+        if (err) {
+          return res
+            .status(500)
+            .json({ error: "음식 기록 데이터 조회 중 오류 발생:" });
+        }
+        res.status(200).json(data);
+      });
+    }
+  } catch (error) {
+    console.error("음식 기록 데이터 조회 중 오류 발생:", error);
+    return res.status(500).json({ message: "서버 내부 오류" });
+  }
+};
+module.exports = { ExerciseGraph, WeightGraph, FoodGraph };
